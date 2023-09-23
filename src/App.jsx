1b0eef8f0d5baa46b17c92f648e14d3fb538f54c
Levent Tarık Koyuncu <42026035+levent1@users.noreply.github.com>
2023-09-23 13:32:33 +0300

    Manager page temel tasarımı

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import { RegisterVisitor } from "./Pages/MainPages/RegisterVisitor";
 import { RegisterManager } from "./Pages/MainPages/RegisterManager";
 import { Login} from "./Pages/MainPages/Login"
 import { Route,Routes } from "react-router";
+import { ManagerPagePersonel } from "./Pages/MainPages/ManagerPagePersonel";
+import { ManagerPendingApproval } from "./Pages/MainPages/ManagerPendingApproval";
+import { ManagerPageProfitLoss } from "./Pages/MainPages/ManagerPageProfitLoss";
+import { ManagerPageCompanyExpenses } from "./Pages/MainPages/ManagerPageCompanyExpenses";
+import { ManagerPageUpcomingPayment } from "./Pages/MainPages/ManagerPageUpcomigPayments";
+import { ManagerPagePublicHoliday } from "./Pages/MainPages/ManagerPagePublicHoliday";
+import { ManagerPagePersonelTraces } from "./Pages/MainPages/ManagerPagePersonelTraces";
+
 
 function App() {
 
@@ -14,6 +22,13 @@ function App() {
     <Route path="/" element={<Login />}></Route>
     <Route path="/registerVisitor" element={<RegisterVisitor />}></Route>
     <Route path="/registerManager" element={<RegisterManager />}></Route>
+    <Route path="/managerpersonel" element={<ManagerPagePersonel />}></Route>
+    <Route path="/managerpendingapproval" element={<ManagerPendingApproval />}></Route>
+    <Route path="/managerprofitloss" element={<ManagerPageProfitLoss />}></Route>
+    <Route path="/managercompanyexpenses" element={<ManagerPageCompanyExpenses />}></Route>
+    <Route path="/managerupcomingpayment" element={<ManagerPageUpcomingPayment />}></Route>
+    <Route path="/managerpublicholiday" element={<ManagerPagePublicHoliday />}></Route>
+    <Route path="/managerpersoneltraces" element={<ManagerPagePersonelTraces />}></Route>
    </Routes>
     
    </>
---
+++ src/Pages/MainPages/ManagerPagePublicHoliday.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import { LogoLogin } from "../../components/LogoLogin";
+import { ManagerControlButtonlari } from "../../components/ManagerControlButtonlari";
+
+export function ManagerPagePublicHoliday(){
+    return(
+        <div className="managerPage">
+
+         <header>
+        <LogoLogin/>
+        </header>
+
+        <main>
+            <div className="managerSideBar">
+            <h4 className="managerSideBarTitle">Yönetici Araç Çubuğu</h4>
+           <ManagerControlButtonlari/>
+           
+            </div>
+            <div className="managerViewSection">
+               <h3>Yönetici:Resmi Tatiller</h3>
+            </div>
+        </main>
+        </div>
+
+     
+     
+      
+        
+    )
+
+}
