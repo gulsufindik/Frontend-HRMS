99b55d7019d910be85dd43e4c267e403676c6099
cankinomad <aktasberk96@gmail.com>
2023-09-27 17:21:38 +0300

    Onay butonu yapildi backend baglantilari yapildi

--- src/components/PendingApprovalCommentTable.jsx
+++ src/components/PendingApprovalCommentTable.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 
 const commentUrl="http://localhost:8080/comment/getallpendingcomments"
+const commentApproveUrl = "http://localhost:8080/user/approvecommentofemployee"
 
+// ================== BACKEND BAGLANTILARI ================
+// ================== listeleme metodu====================
 function fetchAllPendingCommentsMethod(){
     return fetch(commentUrl).then((resp)=>{
         return resp.json();
@@ -9,13 +12,58 @@ function fetchAllPendingCommentsMethod(){
         return data;
     }).catch((err)=>console.log(err.message));
 }
+// =============== Onaylama Metodu ======================
+function fetchCommentApproveMethod(approveData){
+
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(approveData),
+      };
+
+     return fetch(commentApproveUrl,options).then((resp)=>{
+        return resp.json();
+      }).then((data)=>{return data}).catch((err)=>console.log(err.message))
+}
 
 
 export function PendingCommentTable(){
+    // listeleme icin gerekli useStateler
     const[pendingCommentList,setPendingCommentList]=useState([]);
     const[error,setError]=useState(null);
 
+    // Onaylama icin gerekli
+    const[approveData,setApproveData]= useState({
+        userType:localStorage.getItem("userType"),
+        commentId:null,
+    })
+    const[approveError,setApproveError]=useState(null);
+    const [approvedMessage,setApprovedMessage] = useState(null);
 
+    // onaylama butonu handleClick
+    function handleApproveClick(IdOfComment){
+        setApproveData({userType:localStorage.getItem("userType"),commentId:IdOfComment})
+    }
+
+    useEffect(()=>{
+        if(approveData.commentId !== null){
+            console.log(approveData);
+
+            fetchCommentApproveMethod(approveData).then((data)=>{
+                if(data.successMessage){
+                    console.log(data.successMessage)
+                    setApprovedMessage(data.successMessage)
+                }
+                if(data.fields){
+                    setApproveError(data.fields)
+                  }else{
+                    setApproveError(data.message)
+                  }
+            }).catch((err)=>console.log(err.message))
+        }
+    },[approveData])
+
+    // =========== Listeleme MEtodu ===================
     useEffect(()=>{
         fetchAllPendingCommentsMethod().then((data)=>{
             if(Array.isArray(data)){
@@ -29,7 +77,7 @@ export function PendingCommentTable(){
             }
             
         }).catch((err)=>console.log(err.message))
-    },[]);
+    },[approveData]);
 
     return(
         <>
@@ -44,6 +92,7 @@ export function PendingCommentTable(){
                             <th>Yorum</th>
                             <th>Sirket Ismi</th>
                             <th>Kullanici Rolu</th>
+                            <th>Onay/Red</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -52,13 +101,14 @@ export function PendingCommentTable(){
                             <td colSpan="6">Henuz veri yukleniyor...</td>
                         </tr>
                     ) : (
-                        pendingCommentList.map(item => (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.authid}</td>
-                                <td>{item.comment}</td>
-                                <td>{item.companyName}</td>
-                                <td>{item.userType}</td>
+                        pendingCommentList.map(comment => (
+                            <tr key={comment.id}>
+                                <td>{comment.id}</td>
+                                <td>{comment.authid}</td>
+                                <td>{comment.comment}</td>
+                                <td>{comment.companyName}</td>
+                                <td>{comment.userType}</td>
+                                <td><button className="btn-approve" onClick={()=>handleApproveClick(comment.id)}>✔️</button><button className="btn-deny">🗙</button></td>
                             </tr>
                         ))
                     )}
--- src/components/PendingApprovalManagerTable.jsx
+++ src/components/PendingApprovalManagerTable.jsx
@@ -1,20 +1,45 @@
 import { useEffect, useState } from "react";
 
 //==================== URL ===============================
-const pendingApprovalManagerUrl= "http://localhost:8080/user/listpendingmanagerapproval"
+const pendingApprovalListManagerUrl= "http://localhost:8080/user/listpendingmanagerapproval"
+const pendingApprovalManagerUrl = "http://localhost:8080/user/approvemanager"
 
 //==================== Backend Fetch ===============================
+// =========== listeleme metodu ==============
 function pendingApprovalManagerMethod(){
-    return fetch(pendingApprovalManagerUrl).then((resp)=>{
+    return fetch(pendingApprovalListManagerUrl).then((resp)=>{
         return resp.json();
     }).then((data)=>{ return data}).catch((err)=>console.log(err.message))
 }
 
+// ============= Onaylama Metodu =================
+function fetchApprovalManagerMethod(approvedManager){
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(approvedManager),
+      };
+      return fetch(pendingApprovalManagerUrl,options).then((resp)=>{
+        return resp.json();
+      }).then((data)=>{return data}).catch((err)=>console.log(err.message))
+}
+
+
 // ======================= Table ================
 export function PendingApprovalManagerTable(){
+    // listeleme icin gerkeli Hooklar
     const[pendingManagerList,setPendingManagerList]=useState([]);   
     const[error,setError]=useState(null);
 
+    // Onaylama icin gerekli Hooklar
+    const[approvedManager,setApprovedManager]= useState({
+        token:localStorage.getItem("token"),
+        managerAuthId: null,
+    })
+    const[approveError,setApproveError]=useState(null);
+    const [approvedMessage,setApprovedMessage] = useState(null);
+
+    // Listeleme metodu
     useEffect(()=>{
         pendingApprovalManagerMethod().then((data)=>{
             if(Array.isArray(data)){
@@ -29,6 +54,30 @@ export function PendingApprovalManagerTable(){
         }).catch((err)=>console.log(err.message))
     },[])
 
+    // Onaylama Metodu
+    function handleApproveClick(AuthIdOfManager){
+        console.log(AuthIdOfManager);
+        setApprovedManager({ token: localStorage.getItem("token"), managerAuthId: AuthIdOfManager });
+    }
+    
+    useEffect(() => {
+        if (approvedManager.managerAuthId !== null) {
+            console.log(approvedManager);
+    
+            fetchApprovalManagerMethod(approvedManager)
+                .then((data) => {
+                    if (data) {
+                        setApprovedMessage("Başarılı");
+                    }
+                    if (data.fields) {
+                        setApproveError(data.fields);
+                    } else {
+                        setApproveError(data.message);
+                    }
+                })
+                .catch((err) => console.log(err.message));
+        }
+    }, [approvedManager]);
 
     return(
         <>
@@ -46,6 +95,7 @@ export function PendingApprovalManagerTable(){
                             <th>Sahsi Mail</th>
                             <th>Sirket Ismi</th>
                             <th>Kullanici Rolu</th>
+                            <th>Onay/Red</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -64,6 +114,7 @@ export function PendingApprovalManagerTable(){
                                 <td>{manager.personalEmail}</td>
                                 <td>{manager.companyName}</td>
                                 <td>{manager.userType}</td>
+                                <td><button className="btn-approve" onClick={()=>handleApproveClick(manager.authid)}>✔️</button><button className="btn-deny">🗙</button></td>
                             </tr>
                         ))
                     )}
